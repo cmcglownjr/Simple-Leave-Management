ed79896806beef3898d1d92daa5c5e8faa19a813
Chris McGlown <cmcglown@tutamail.com>
2022-07-20 20:21:34 -0400

    Initial Commit

---
+++ SimpleLeaveManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimpleLeaveManagement.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
