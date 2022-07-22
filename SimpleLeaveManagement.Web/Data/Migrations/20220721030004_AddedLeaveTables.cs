010bb7fd21fe52fe9ce95eeb78c1241896a5b797
Chris McGlown <cmcglown@tutamail.com>
2022-07-21 23:01:30 -0400

    Fixed error with My Leave cancel request.

--- SimpleLeaveManagement.Web/Data/Migrations/20220721030004_AddedLeaveTables.cs
+++ SimpleLeaveManagement.Web/Data/Migrations/20220721030004_AddedLeaveTables.cs
@@ -45,7 +45,7 @@ namespace SimpleLeaveManagement.Web.Data.Migrations
                         column: x => x.LeaveTypeId,
                         principalTable: "LeaveTypes",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
